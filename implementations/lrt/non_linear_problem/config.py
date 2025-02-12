bb142c1aa5cfa54167f63123c853ed11ba0a8e07
eirihoyh <eirik.hoy@gmail.com>
2025-02-12 15:34:20 +0100

    half of the results non-linear problem lrt

--- implementations/lrt/non_linear_problem/config.py
+++ implementations/lrt/non_linear_problem/config.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# NOTE: Used lambda min=-5 and lambda max=-1
 config = {}
-config['num_epochs' ] = 1000  
+config['num_epochs' ] = 500  
 config['lr'] = .01
 config['post_train_epochs'] = 0  # If post training, then only MPM will be trained
 config['patience'] = 10000
@@ -13,13 +12,12 @@ config['num_transforms'] = 2  # For normalizing flows
 config['class_problem'] = True  # If classification problem or not
 config['inclusion_prob_prior'] = 0.01
 config['std_prior'] = 30.
-config['lower_init_lambda'] = -5
-config['upper_init_lambda'] = -1
-config['high_init_covariate_prob'] = True
+config['lower_init_lambda'] = -9
+config['upper_init_lambda'] = -4
+config['high_init_covariate_prob'] = True # If true, the inital covariate probs for the covariates will be set to lambda=5
 # For the linear and non-linear problem
 config['n_samples'] = 4*10**4
 config['non_lin'] = True  # Wanting to create a non-linear or linear dataset 
-config['test_samples'] = 5000
 
 config['verbose'] = True  # If we want printouts during/after training or not
 config['save_res'] = True  # If we should save the results
--- implementations/lrt/non_linear_problem/non_linear_problem.py
+++ implementations/lrt/non_linear_problem/non_linear_problem.py
@@ -35,139 +35,154 @@ save_res = config['save_res']
 patience = config['patience']
 alpha_prior = config['inclusion_prob_prior']
 std_prior = config['std_prior']
-SAMPLES = 1
+lower_init_lambda = config['lower_init_lambda']
+upper_init_lambda = config['upper_init_lambda']
+high_init_covariate_prob = config['high_init_covariate_prob']
 
 
+dep_levels = [0.0,0.1,0.5,0.9]
+
+for dep in dep_levels:
+
+    # Get linear data
+    X_train_original = np.loadtxt(f"data/non-linear/{dep}X_train.txt", delimiter=",")
+    X_test_original = np.loadtxt(f"data/non-linear/{dep}X_test.txt", delimiter=",")
+    y_train_original = np.loadtxt(f"data/non-linear/{dep}Y_train.txt", delimiter=",")
+    y_test_original = np.loadtxt(f"data/non-linear/{dep}Y_test.txt", delimiter=",")
+
+    n, p = X_train_original.shape  # need this to get p 
+    print(n,p,dim)
+
+    if class_problem:
+        n_classes = len(np.unique(y_train_original))
+        if n_classes == 2:
+            n_classes = 1
+        multiclass = n_classes > 1
+    else: 
+        n_classes = 1  # Just need to set it to something above zero
+        multiclass = False
+
+    BATCH_SIZE = int((n)/50)
+    TRAIN_SIZE = int((n))
 
 
+    NUM_BATCHES = TRAIN_SIZE/BATCH_SIZE
 
-# Get linear data
-X_train_original = np.loadtxt("data/non-linear/0.0X_train.txt", delimiter=",")
-X_test_original = np.loadtxt("data/non-linear/0.0X_test.txt", delimiter=",")
-y_train_original = np.loadtxt("data/non-linear/0.0Y_train.txt", delimiter=",")
-y_test_original = np.loadtxt("data/non-linear/0.0Y_test.txt", delimiter=",")
+    print(NUM_BATCHES)
 
-n, p = X_train_original.shape  # need this to get p 
-print(n,p,dim)
+    train_dat = torch.tensor(np.column_stack((X_train_original,y_train_original)),dtype = torch.float32)
+    test_dat = torch.tensor(np.column_stack((X_test_original,y_test_original)),dtype = torch.float32)
 
-if class_problem:
-    n_classes = len(np.unique(y_train_original))
-    if n_classes == 2:
-        n_classes = 1
-    multiclass = n_classes > 1
-else: 
-    n_classes = 1  # Just need to set it to something above zero
-    multiclass = False
+    tot_rounds = epochs + post_train_epochs
 
-BATCH_SIZE = int((n)/50)
-TRAIN_SIZE = int((n))
+    # select the device and initiate model
 
-
-NUM_BATCHES = TRAIN_SIZE/BATCH_SIZE
-
-print(NUM_BATCHES)
-
-train_dat = torch.tensor(np.column_stack((X_train_original,y_train_original)),dtype = torch.float32)
-test_dat = torch.tensor(np.column_stack((X_test_original,y_test_original)),dtype = torch.float32)
-
-tot_rounds = epochs + post_train_epochs
-
-# select the device and initiate model
-
-# DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "mps")
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-LOADER_KWARGS = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
-all_nets = {}
-metrics_several_runs = []
-metrics_median_several_runs = []
-for ni in range(n_nets):
-    post_train = False
-    print('network', ni)
-    # Initate network
-    torch.manual_seed(ni+42)
-    #---------------------------
-    # DIFFERENCE IS IN act_func=F.relu part
-    net = BayesianNetwork(dim, p, HIDDEN_LAYERS, classification=class_problem, a_prior=alpha_prior, std_prior=std_prior, act_func=F.sigmoid).to(DEVICE)
-    #---------------------------
-    alphas = pip_func.get_alphas_numpy(net)
-    nr_weights = np.sum([np.prod(a.shape) for a in alphas])
-    print(nr_weights)
-    
-    params = []
-    for name, param in net.named_parameters():
-        if f"lambdal" in name:
-            alpha_lr = {'params': param, 'lr': 1.5}
-            params.append(alpha_lr)
-        else:
-            param_lr = {'params': param, 'lr': lr}
-            params.append(param_lr)
-
-    # print(params)
-    # optimizer = optim.Adam(net.parameters(), lr=lr)
-    optimizer = optim.Adam(params, lr=lr)
-    scheduler = MultiStepLR(optimizer, milestones=[int(0.5*tot_rounds), int(0.7*tot_rounds), int(0.9*tot_rounds)], gamma=0.5)  # Reduce lr as the epochs increases int(0.2*tot_rounds),int(0.4*tot_rounds),int(0.6*tot_rounds),int(0.7*tot_rounds)
-    
-    all_nll = []
-    all_loss = []
-    
-    # Train network
-    counter = 0
-    highest_acc = 0
-    best_model = copy.deepcopy(net)
-    for epoch in range(tot_rounds):
-        if epoch < 0:
-            NUM_BATCHES = 1
-        else:
-            NUM_BATCHES = NUM_BATCHES
-
-        if verbose:
-            print(epoch)
-        nll, loss = pip_func.train(net, train_dat, optimizer, BATCH_SIZE, NUM_BATCHES, p, DEVICE, nr_weights, post_train=post_train)
-        nll_val, loss_val, ensemble_val = pip_func.val(net, test_dat, DEVICE, verbose=verbose, reg=(not class_problem))
-        if ensemble_val >= highest_acc:
-            counter = 0
-            highest_acc = ensemble_val
-            best_model = copy.deepcopy(net)
-        else:
-            counter += 1
+    # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "mps")
+    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    LOADER_KWARGS = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+    all_nets = {}
+    metrics_several_runs = []
+    metrics_median_several_runs = []
+    for ni in range(n_nets):
+        post_train = False
+        print('network', ni)
+        # Initate network
+        torch.manual_seed(ni+42)
+        #---------------------------
+        # DIFFERENCE IS IN act_func=F.relu part
+        net = BayesianNetwork(
+            dim, 
+            p, 
+            HIDDEN_LAYERS, 
+            classification=class_problem, 
+            a_prior=alpha_prior, 
+            std_prior=std_prior, 
+            n_classes=n_classes, 
+            act_func=F.sigmoid,
+            lower_init_lambda=lower_init_lambda,
+            upper_init_lambda=upper_init_lambda,
+            high_init_covariate_prob=high_init_covariate_prob).to(DEVICE)
+        #---------------------------
+        alphas = pip_func.get_alphas_numpy(net)
+        nr_weights = np.sum([np.prod(a.shape) for a in alphas])
+        print(nr_weights)
         
-        all_nll.append(nll)
-        all_loss.append(loss)
+        params = []
+        for name, param in net.named_parameters():
+            if f"lambdal" in name:
+                alpha_lr = {'params': param, 'lr': .1}
+                params.append(alpha_lr)
+            else:
+                param_lr = {'params': param, 'lr': lr}
+                params.append(param_lr)
 
-        if epoch == epochs-1:
-            post_train = True   # Post-train --> use median model 
-            for name, param in net.named_parameters():
-                for i in range(HIDDEN_LAYERS+1):
-                    if f"linears.{i}.lambdal" in name:
-                        param.requires_grad_(False)
-
-        if counter >= patience:
-            break
-
-        scheduler.step()
+        # print(params)
+        optimizer = optim.Adam(params, lr=lr)
+        # optimizer = optim.Adam(net.parameters(), lr=lr)
         
-    all_nets[ni] = net 
-    # Results
+        scheduler = MultiStepLR(optimizer, milestones=[int(0.5*tot_rounds), int(0.7*tot_rounds), int(0.9*tot_rounds)], gamma=0.5)  # Reduce lr as the epochs increases int(0.2*tot_rounds),int(0.4*tot_rounds),int(0.6*tot_rounds),int(0.7*tot_rounds)
+        
+        all_nll = []
+        all_loss = []
+        
+        # Train network
+        counter = 0
+        highest_acc = 0
+        best_model = copy.deepcopy(net)
+        for epoch in range(tot_rounds):
+            if epoch < 0:
+                NUM_BATCHES = 1
+            else:
+                NUM_BATCHES = NUM_BATCHES
+
+            if verbose:
+                print(epoch)
+            nll, loss = pip_func.train(net, train_dat, optimizer, BATCH_SIZE, NUM_BATCHES, p, DEVICE, nr_weights, post_train=post_train)
+            nll_val, loss_val, ensemble_val = pip_func.val(net, test_dat, DEVICE, verbose=verbose, reg=(not class_problem))
+            if ensemble_val >= highest_acc:
+                counter = 0
+                highest_acc = ensemble_val
+                best_model = copy.deepcopy(net)
+            else:
+                counter += 1
+            
+            all_nll.append(nll)
+            all_loss.append(loss)
+
+            if epoch == epochs-1:
+                post_train = True   # Post-train --> use median model 
+                for name, param in net.named_parameters():
+                    for i in range(HIDDEN_LAYERS+1):
+                        if f"linears.{i}.lambdal" in name:
+                            param.requires_grad_(False)
+
+            if counter >= patience:
+                break
+
+            scheduler.step()
+            
+        all_nets[ni] = net 
+        # Results
+        if save_res:
+            torch.save(net, f"implementations/lrt/non_linear_problem/network/net{ni}_dep_level_{int(100*dep)}")
+        metrics, metrics_median = pip_func.test_ensemble(all_nets[ni], test_dat, DEVICE, SAMPLES=100, CLASSES=1, reg=(not class_problem)) # Test same data 10 times to get average 
+        metrics_several_runs.append(metrics)
+        metrics_median_several_runs.append(metrics_median)
+        pf.run_path_graph(all_nets[ni], threshold=0.5, save_path=f"implementations/lrt/non_linear_problem/path_graphs/prob/net{ni}_dep_level_{int(100*dep)}_sigmoid", show=False)
+        pf.run_path_graph_weight(net, save_path=f"implementations/lrt/non_linear_problem/path_graphs/weight/net{ni}_dep_level_{int(100*dep)}_sigmoid", show=False)
+
+    if verbose:
+        print(metrics)
+    m = np.array(metrics_several_runs)
+    m_median = np.array(metrics_median_several_runs)
+
+    print("Results full model:")
+    print(m)
+    print("\n\nResults median prob model:")
+    print(m_median)
+
     if save_res:
-        torch.save(net, f"implementations/lrt/non_linear_problem/network/net{ni}")
-    metrics, metrics_median = pip_func.test_ensemble(all_nets[ni], test_dat, DEVICE, SAMPLES=100, CLASSES=1, reg=(not class_problem)) # Test same data 10 times to get average 
-    metrics_several_runs.append(metrics)
-    metrics_median_several_runs.append(metrics_median)
-    pf.run_path_graph(all_nets[ni], threshold=0.5, save_path=f"implementations/lrt/non_linear_problem/path_graphs/prob/net{ni}_sigmoid", show=False)
-    pf.run_path_graph_weight(net, save_path=f"implementations/lrt/non_linear_problem/path_graphs/weight/net{ni}_sigmoid", show=False)
-
-if verbose:
-    print(metrics)
-m = np.array(metrics_several_runs)
-m_median = np.array(metrics_median_several_runs)
-
-print("Results full model:")
-print(m)
-print("\n\nResults median prob model:")
-print(m_median)
-
-if save_res:
-    # m = np.array(metrics_several_runs)
-    np.savetxt(f'implementations/lrt/non_linear_problem/results/lrt_class_skip_{HIDDEN_LAYERS}_hidden_{dim}_dim_{epochs}_epochs_{lr}_lr_non_lin_func_sigmoid_full.txt',m,delimiter = ',')
-    # m_median = np.array(metrics_median_several_runs)
-    np.savetxt(f'implementations/lrt/non_linear_problem/results/lrt_class_skip_{HIDDEN_LAYERS}_hidden_{dim}_dim_{epochs}_epochs_{lr}_lr_non_lin_func_sigmoid_median.txt',m_median,delimiter = ',')
+        # m = np.array(metrics_several_runs)
+        np.savetxt(f'implementations/lrt/non_linear_problem/results/lrt_class_skip_{HIDDEN_LAYERS}_hidden_{dim}_dim_{epochs}_epochs_{lr}_lr_non_lin_func_dep_level_{int(100*dep)}_sigmoid_full.txt',m,delimiter = ',')
+        # m_median = np.array(metrics_median_several_runs)
+        np.savetxt(f'implementations/lrt/non_linear_problem/results/lrt_class_skip_{HIDDEN_LAYERS}_hidden_{dim}_dim_{epochs}_epochs_{lr}_lr_non_lin_func_dep_level_{int(100*dep)}_sigmoid_median.txt',m_median,delimiter = ',')
