22b24fc9e21aefa37807493639ca6785d82c3fb8
eirihoyh <eirik.hoy@gmail.com>
2025-02-16 20:41:28 +0100

    look into result notebooks lrt

---
+++ implementations/lrt/mice/look_into_res_mice.py
@@ -0,0 +1,151 @@
+import copy
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.optim.lr_scheduler import MultiStepLR
+from sklearn.model_selection import train_test_split
+from config import config
+import os
+import sys
+current_dir = os.getcwd()
+
+sys.path.append('islbbnn')
+import plot_functions as pf
+import pipeline_functions as pip_func
+sys.path.append('islbbnn/networks')
+from lrt_net import BayesianNetwork
+import torch.nn.functional as F
+
+os.chdir(current_dir) # set the working directory back to this one 
+
+# --------------mice protein----------------
+HIDDEN_LAYERS = config['n_layers'] - 2 
+n_nets = config['n_nets']
+class_problem = config["class_problem"]
+# Get linear data
+X_test_original = np.loadtxt("data/mice/X_test.txt", delimiter=",")
+y_test_original = np.loadtxt("data/mice/Y_test.txt", delimiter=",")
+
+n, p = X_test_original.shape  # need this to get p 
+
+if class_problem:
+    n_classes = len(np.unique(y_test_original))
+    if n_classes == 2:
+        n_classes = 1
+    multiclass = n_classes > 1
+else: 
+    n_classes = 1  # Just need to set it to something above zero
+    multiclass = False
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+test_dat = torch.tensor(np.column_stack((X_test_original,y_test_original)),dtype = torch.float32)
+
+
+
+path = "implementations/lrt/mice/"
+
+
+# test_res = []
+# for i in range(n_nets):
+#     net = torch.load(path+f"network/net{i}", weights_only=False,map_location=torch.device('cpu'))
+#     pf.save_metrics(net, path=path+f"results/net{i}")
+#     ece_full, ece_median, nll_full, nll_median = pip_func.get_ece_score(net, test_dat, device, n_samples=100, n_classes=n_classes)
+#     test_res.append([ece_full, ece_median, nll_full.cpu().detach().numpy(), nll_median.cpu().detach().numpy()])
+
+
+# test_res = np.array(test_res)
+# np.save(path+"results/ece", test_res)
+
+
+test_res = np.load(path+"results/ece.npy", allow_pickle=True)
+print("Order: ece_full, ece_median, nll_full, nll_median")
+print(f"Median: {np.median(test_res,0)}")
+print(f"Min:    {np.min(test_res,0)}")
+print(f"Max:    {np.max(test_res,0)}")
+print("")
+
+
+res = {}
+data = np.loadtxt(path+f"results/lrt_class_skip_4_hidden_100_dim_2000_epochs_0.01_lr_mice_sigmoid_full.txt", delimiter=",")
+data_median = np.loadtxt(path+f"results/lrt_class_skip_4_hidden_100_dim_2000_epochs_0.01_lr_mice_sigmoid_median.txt", delimiter=",")
+res["median_acc_full"] = np.median(data, 0)[0]
+res["min_acc_full"] = np.min(data, 0)[0]
+res["max_acc_full"] = np.max(data, 0)[0]
+
+res["median_acc_median"] = np.median(data_median, 0)[0]
+res["min_acc_median"] = np.min(data_median, 0)[0]
+res["max_acc_median"] = np.max(data_median, 0)[0]
+
+res["median_density"] = np.median(data, 0)[1]
+res["min_density"] = np.min(data, 0)[1]
+res["max_density"] = np.max(data, 0)[1]
+
+res["median_used_weights"] = np.median(data_median, 0)[1]
+res["min_used_weights"] = np.min(data_median, 0)[1]
+res["max_used_weights"] = np.max(data_median, 0)[1]
+
+for k in res.keys():
+    print(f"{k}: {res[k]}")
+
+print("")
+
+res = {}
+res["used_weights"] = []
+
+for n in range(n_nets):
+    median_info = np.load(path+f"results/net{n}_median.npy", allow_pickle=True).item()
+    res["used_weights"].append(median_info["used_weights"])
+
+print("USED WEIGHTS:")
+m = (res["used_weights"])
+print(f"median: {np.median(m)}")
+print(f"min: {np.min(m)}")
+print(f"max: {np.max(m)}\n")
+
+
+
+
+res = {}
+res["median_depth"] = []
+res["max_width"] = []
+
+for n in range(n_nets):
+    median_info = np.load(path+f"results/net{n}_median.npy", allow_pickle=True).item()
+    res["median_depth"].append(median_info["avg_path_length"])
+    search = True
+    i = 0
+    while search:
+        search = np.sum(median_info["include_inputs"][i]) == 0
+        i += 1
+
+    res["max_width"].append(HIDDEN_LAYERS-i+2)
+
+print("Avg. depth")
+print(np.median(res["median_depth"]))
+print(np.min(res["median_depth"]))
+print(np.max(res["median_depth"]))
+print("")
+
+print("Max depth")
+print(np.median(res["max_width"]))
+print(np.min(res["max_width"]))
+print(np.max(res["max_width"]))
+print("")
+
+res = {}
+for i in range(p):
+    res[f"x{i}_inclution_rate"] = 0
+    
+    for n in range(n_nets):
+        median_info = np.load(path+f"results/net{n}_median.npy", allow_pickle=True).item()
+        #res[d][f"x{i}_inclution_rate"] += (median_info["expected_depth_input"][i]>0)
+        count = 0
+        for j in median_info["include_inputs"]:
+            count += j[i]
+        res[f"x{i}_inclution_rate"] += (count>0)
+    res[f"x{i}_inclution_rate"] /= n_nets
+    if res[f"x{i}_inclution_rate"] > 0:
+        print(f"x{i}_inclution_rate: {res[f'x{i}_inclution_rate']}")
+
+# for k in res.keys():
+#     print(f"{k}: {res[k]}")
---
+++ implementations/lrt/non_linear_problem/look_into_res_non_linear_problem.py
@@ -0,0 +1,179 @@
+import copy
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.optim.lr_scheduler import MultiStepLR
+from sklearn.model_selection import train_test_split
+from config import config
+import os
+import sys
+current_dir = os.getcwd()
+
+sys.path.append('islbbnn')
+import plot_functions as pf
+import pipeline_functions as pip_func
+sys.path.append('islbbnn/networks')
+from lrt_net import BayesianNetwork
+import torch.nn.functional as F
+
+os.chdir(current_dir) # set the working directory back to this one 
+
+# --------------Non-linear problem----------------
+HIDDEN_LAYERS = config['n_layers'] - 2 
+n_nets = config['n_nets']
+class_problem = config["class_problem"]
+
+# Get linear data
+X_test_original = np.loadtxt(f"data/non-linear/0.0X_test.txt", delimiter=",")
+y_test_original = np.loadtxt(f"data/non-linear/0.0Y_test.txt", delimiter=",")
+
+n, p = X_test_original.shape  # need this to get p 
+
+if class_problem:
+    n_classes = len(np.unique(y_test_original))
+    if n_classes == 2:
+        n_classes = 1
+    multiclass = n_classes > 1
+else: 
+    n_classes = 1  # Just need to set it to something above zero
+    multiclass = False
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+test_dat = torch.tensor(np.column_stack((X_test_original,y_test_original)),dtype = torch.float32)
+
+
+path = "implementations/lrt/non_linear_problem/"
+
+
+# dep_levels = [0.0,0.1,0.5,0.9]
+# for d in dep_levels:
+#     test_res = []
+#     X_test_original = np.loadtxt(f"data/non-linear/{d}X_test.txt", delimiter=",")
+#     y_test_original = np.loadtxt(f"data/non-linear/{d}Y_test.txt", delimiter=",")
+#     test_dat = torch.tensor(np.column_stack((X_test_original,y_test_original)),dtype = torch.float32)
+#     for i in range(n_nets):
+#         net = torch.load(path+f"network/net{i}_dep_level_{int(100*d)}", weights_only=False,map_location=torch.device('cpu'))
+#         pf.save_metrics(net, path=path+f"results/net{i}_dep_level_{int(100*d)}")
+#         ece_full, ece_median, nll_full, nll_median = pip_func.get_ece_score(net, test_dat, device, n_samples=100, n_classes=n_classes)
+#         test_res.append([ece_full, ece_median, nll_full.cpu().detach().numpy(), nll_median.cpu().detach().numpy()])
+
+#     test_res = np.array(test_res)
+#     np.save(path+f"results/ece_dep_level_{int(100*d)}", test_res)
+
+dep_levels = [0,10,50,90]
+for d in dep_levels:
+    print(f"Dep level: {d}")
+    test_res = np.load(path+f"results/ece_dep_level_{d}.npy", allow_pickle=True)
+    print("Order: ece_full, ece_median, nll_full, nll_median")
+    print(f"Median: {np.median(test_res,0)}")
+    print(f"Min:    {np.min(test_res,0)}")
+    print(f"Max:    {np.max(test_res,0)}")
+    print("")
+
+
+res = {}
+for d in dep_levels:
+    data = np.loadtxt(path+f"results/lrt_class_skip_4_hidden_20_dim_500_epochs_0.01_lr_non_lin_func_dep_level_{d}_sigmoid_full.txt", delimiter=",")
+    data_median = np.loadtxt(path+f"results/lrt_class_skip_4_hidden_20_dim_500_epochs_0.01_lr_non_lin_func_dep_level_{d}_sigmoid_median.txt", delimiter=",")
+    res[d] = {}
+    res[d]["median_acc_full"] = np.median(data, 0)[0]
+    res[d]["min_acc_full"] = np.min(data, 0)[0]
+    res[d]["max_acc_full"] = np.max(data, 0)[0]
+
+    res[d]["median_acc_median"] = np.median(data_median, 0)[0]
+    res[d]["min_acc_median"] = np.min(data_median, 0)[0]
+    res[d]["max_acc_median"] = np.max(data_median, 0)[0]
+
+    res[d]["median_density"] = np.median(data, 0)[1]
+    res[d]["min_density"] = np.min(data, 0)[1]
+    res[d]["max_density"] = np.max(data, 0)[1]
+
+    res[d]["median_used_weights"] = np.median(data_median, 0)[1]
+    res[d]["min_used_weights"] = np.min(data_median, 0)[1]
+    res[d]["max_used_weights"] = np.max(data_median, 0)[1]
+    print("")
+
+
+print(list(res.keys()))
+for d in dep_levels:
+    print(f"Dep level {d}")
+    for k in res[d].keys():
+        print(f"{k}: {res[d][k]}")
+
+
+res = {}
+res["used_weights"] = []
+
+dep_levels = [0,10,50,90]
+res = {}
+for d in dep_levels:
+    res[d] = {}
+    res[d]["used_weights"] = []
+    
+    for n in range(n_nets):
+        median_info = np.load(path+f"results/net{n}_dep_level_{d}_median.npy", allow_pickle=True).item()
+        res[d]["used_weights"].append(median_info["used_weights"])
+
+print("USED WEIGHTS:")
+for d in dep_levels:
+    m = (res[d]["used_weights"])
+    print(f"Dep level: {d}")
+    print(f"median: {np.median(m)}")
+    print(f"min: {np.min(m)}")
+    print(f"max: {np.max(m)}\n")
+
+
+res = {}
+for d in dep_levels:
+    res[d] = {}
+    res[d]["median_depth"] = []
+    res[d]["max_width"] = []
+    
+    for n in range(n_nets):
+        median_info = np.load(path+f"results/net{n}_dep_level_{d}_median.npy", allow_pickle=True).item()
+        res[d]["median_depth"].append(median_info["avg_path_length"])
+        search = True
+        i = 0
+        while search:
+            search = np.sum(median_info["include_inputs"][i]) == 0
+            i += 1
+
+        res[d]["max_width"].append(HIDDEN_LAYERS-i+2)
+
+print("Avg. depth")
+for r in res:
+    print(f"Dependence: {r}")
+    print(np.median(res[r]["median_depth"]))
+    print(np.min(res[r]["median_depth"]))
+    print(np.max(res[r]["median_depth"]))
+    print("")
+
+print("Max depth")
+for r in res:
+    print(f"Dependence: {r}")
+    print(np.median(res[r]["max_width"]))
+    print(np.min(res[r]["max_width"]))
+    print(np.max(res[r]["max_width"]))
+    print("")
+
+
+
+res = {}
+for d in dep_levels:
+    res[d] = {}
+    for i in range(p):
+        res[d][f"x{i+1}_inclution_rate"] = 0
+        
+        for n in range(n_nets):
+            median_info = np.load(path+f"results/net{n}_dep_level_{d}_median.npy", allow_pickle=True).item()
+            count = 0
+            for j in median_info["include_inputs"]:
+                count += j[i]
+
+            res[d][f"x{i+1}_inclution_rate"] += (count>0)
+        res[d][f"x{i+1}_inclution_rate"] /= n_nets
+
+for d in dep_levels:
+    print(f"Dep level: {d}")
+    for k in res[d].keys():
+        print(f"{k}: {res[d][k]}")
