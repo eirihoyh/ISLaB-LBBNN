b003061131d6c89fe667dd1d6e63aa2367572fa1
eirihoyh <eirik.hoy@gmail.com>
2025-07-03 13:52:26 +0200

    added BCNN with input-skip on fully connected part

---
+++ islbbnn/networks/layers/lrt_layer_cnn.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "mps")
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class BayesianConv2d(nn.Module):
+    def __init__(
+            self, 
+            in_channels, 
+            out_channels, 
+            kernel_size,
+            padding=0, 
+            stride=1,
+            lower_init_lambda=-10, 
+            upper_init_lambda=15, 
+            a_prior=0.1, 
+            std_prior=1.):
+        super().__init__()
+
+        self.stride = stride
+        self.padding = padding
+
+        if type(kernel_size) == int:
+            kernel = (kernel_size, kernel_size)
+
+        elif type(kernel_size) == tuple:
+            kernel = kernel_size
+
+        # weight parameters
+        self.weight_mu = nn.Parameter(torch.Tensor(out_channels, in_channels, kernel[0], kernel[1]).uniform_(-1.2, 1.2))
+        self.weight_rho = nn.Parameter(-9 + 0.1 * torch.randn(out_channels, in_channels, kernel[0], kernel[1]))
+        self.weight_sigma = torch.empty(self.weight_rho.shape)
+
+        # weight priors
+        self.mu_prior = torch.zeros((out_channels, in_channels, kernel[0], kernel[1]), device=DEVICE)
+        self.sigma_prior = (self.mu_prior + std_prior).to(DEVICE)
+
+        # model parameters
+        self.lambdal = nn.Parameter(torch.Tensor(out_channels, in_channels, kernel[0], kernel[1]).uniform_(lower_init_lambda, upper_init_lambda))
+        self.alpha_q = torch.empty(self.lambdal.shape)
+
+        # model prior
+        self.alpha_prior = (self.mu_prior + a_prior).to(DEVICE)
+
+        # bias parameters
+        self.bias_mu = nn.Parameter(torch.Tensor(out_channels).uniform_(-0.2, 0.2))
+        self.bias_rho = nn.Parameter(-9 + 1. * torch.randn(out_channels))
+        self.bias_sigma = torch.empty(self.bias_rho.shape)
+
+        # bias prior
+        self.bias_mu_prior = torch.zeros(out_channels, device=DEVICE)
+        self.bias_sigma_prior = (self.bias_mu_prior + std_prior).to(DEVICE)
+
+        # scalars
+        self.kl = 0
+
+    def forward(self, input, ensemble=False, sample=False, calculate_log_probs=False, post_train=False):
+        self.alpha_q = 1 / (1 + torch.exp(-self.lambdal))
+        if post_train:
+            self.alpha_q = (self.alpha_q.detach() > 0.5) * 1
+            self.alpha_prior[self.alpha_q.detach() < 0.5] = 0. 
+        self.weight_sigma = torch.log1p(torch.exp(self.weight_rho))
+        self.bias_sigma = torch.log1p(torch.exp(self.bias_rho))
+        if self.training or ensemble:
+            w_mean = self.weight_mu * self.alpha_q
+            w_var = self.alpha_q * (self.weight_sigma ** 2 + (1 - self.alpha_q) * self.weight_mu ** 2)
+            psi = F.conv2d(input, weight=w_mean, bias=self.bias_mu, stride=self.stride, padding=self.padding)
+            delta = F.conv2d(input ** 2, weight=w_var, bias=self.bias_sigma ** 2, stride=self.stride, padding=self.padding)
+            zeta = torch.randn_like(delta)
+            activations = psi + torch.sqrt(delta) * zeta
+
+        else: # median probability model
+            if sample:
+                w = torch.normal(self.weight_mu,self.weight_sigma)
+                bias = torch.normal(self.bias_mu,self.bias_sigma)
+            else:
+                w = self.weight_mu
+                bias = self.bias_mu
+            g = (self.alpha_q.detach() > 0.5) * 1.
+            weight = w * g
+            activations = F.conv2d(input, weight=weight, bias=bias, stride=self.stride, padding=self.padding)
+
+            if calculate_log_probs:
+                self.alpha_q = g
+        
+
+        if self.training or calculate_log_probs:
+
+            kl_bias = (torch.log(self.bias_sigma_prior / self.bias_sigma) - 0.5 + (self.bias_sigma ** 2
+                    + (self.bias_mu - self.bias_mu_prior) ** 2) / (
+                               2 * self.bias_sigma_prior ** 2)).sum()
+
+            kl_weight = (self.alpha_q * (torch.log(self.sigma_prior / self.weight_sigma)
+                                         - 0.5 + torch.log(self.alpha_q / self.alpha_prior)
+                                         + (self.weight_sigma ** 2 + (self.weight_mu - self.mu_prior) ** 2) / (
+                                                 2 * self.sigma_prior ** 2))
+                         + (1 - self.alpha_q) * torch.log((1 - self.alpha_q) / (1 - self.alpha_prior))).sum()
+
+            self.kl = kl_bias + kl_weight
+
+        else:
+            self.kl = 0
+        # propogate
+        return activations
---
+++ islbbnn/networks/lrt_cnn_net.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.lrt_layers import BayesianLinear
+from layers.lrt_layer_cnn import BayesianConv2d
+
+def compute_output_dimensions(
+    n_layers, 
+    input_width, 
+    input_height, 
+    kernel_size, 
+    stride,
+    padding):
+    """
+    Computes the output width and height for output layer in a CNN.
+
+    Parameters:
+    - n_layers: int
+    - input_width: int
+    - input_height: int
+    - kernel_size: int
+    - padding: int
+    - stride: int
+    
+    
+
+    Returns:
+    - output_width, output_height for final layer
+    """
+    width, height = input_width, input_height
+    for _ in range(n_layers):
+        width = (width - kernel_size + 2 * padding) // stride + 1
+        height = (height - kernel_size + 2 * padding) // stride + 1
+
+    return width, height
+
+
+class BCNN(nn.Module):
+    def __init__(
+            self, 
+            init_in_channels,
+            out_channel_list,
+            kernel_size,
+            stride,
+            padding,
+            p1, # number of pixel rows
+            p2, # number of pixel columns
+            dim,
+            hidden_layers,
+            a_prior=0.05, 
+            std_prior=2.5, 
+            n_classes=1, 
+            act_func=F.sigmoid, 
+            lower_init_lambda=-10, 
+            upper_init_lambda=-7,
+            high_init_covariate_prob=False
+            ):
+        super().__init__()
+        self.init_in_channels = init_in_channels
+        self.p1 = p1
+        self.p2 = p2
+        self.p = p1*p2*init_in_channels
+        self.multiclass = n_classes > 1
+        self.act = act_func
+        if high_init_covariate_prob:
+            nr_var = self.p
+        else:
+            nr_var = None
+        # set the architecture
+
+        # cnn part
+        self.convs = nn.ModuleList([BayesianConv2d(in_channels=init_in_channels, out_channels=out_channel_list[0], kernel_size=kernel_size, stride=stride, padding=padding, lower_init_lambda=lower_init_lambda, upper_init_lambda=upper_init_lambda, a_prior=a_prior, std_prior=std_prior)])
+        self.convs.extend([BayesianConv2d(in_channels=out_channel_list[i], out_channels=out_channel_list[i+1], kernel_size=kernel_size, stride=stride, padding=padding, lower_init_lambda=lower_init_lambda, upper_init_lambda=upper_init_lambda, a_prior=a_prior, std_prior=std_prior) for i in range(len(out_channel_list)-1)])
+        
+        width_out, height_out = compute_output_dimensions(len(self.convs), p1, p2, kernel_size, stride, padding)
+        cnn_output_dim = out_channel_list[-1]*width_out*height_out
+        # ISLaB part
+        self.linears = nn.ModuleList([BayesianLinear((cnn_output_dim+self.p), dim, a_prior=a_prior, std_prior=std_prior, lower_init_lambda=lower_init_lambda, upper_init_lambda=upper_init_lambda, p=nr_var)])
+        self.linears.extend([BayesianLinear((dim+self.p), (dim), a_prior=a_prior, std_prior=std_prior, lower_init_lambda=lower_init_lambda, upper_init_lambda=upper_init_lambda, p=nr_var) for _ in range(hidden_layers-1)])
+        self.linears.append(BayesianLinear((dim+self.p), n_classes, a_prior=a_prior, std_prior=std_prior, lower_init_lambda=lower_init_lambda, upper_init_lambda=upper_init_lambda, p=nr_var))
+        if not self.multiclass: 
+            self.loss = nn.BCELoss(reduction='sum') # Setup loss (Binary cross entropy as binary classification)
+        else:
+            self.loss = nn.NLLLoss(reduction='sum')
+
+
+    def forward(self, x, sample=False, ensemble=False, calculate_log_probs=False, post_train=False):
+        x_input = x
+        x = x.view(-1, self.init_in_channels, self.p1, self.p2)
+        for conv in self.convs:
+            x = self.act(conv(x, ensemble, sample, calculate_log_probs, post_train))
+        
+        x = x.flatten(1)
+
+        for l in self.linears[:-1]:
+            x = self.act(l(torch.cat((x, x_input),1), ensemble, sample, calculate_log_probs, post_train))
+
+        if self.multiclass:
+            out = F.log_softmax(self.linears[-1](torch.cat((x, x_input),1), ensemble, sample, calculate_log_probs, post_train), dim=1)
+        else:
+            out = torch.sigmoid(self.linears[-1](torch.cat((x, x_input),1), ensemble, sample, calculate_log_probs, post_train))
+        
+        return out
+    
+    def forward_preact(self, x, sample=False, ensemble=False, calculate_log_probs=False, post_train=False):
+        x_input = x
+        x = x.view(-1, self.init_in_channels, self.p1, self.p2)
+        for conv in self.convs:
+            x = self.act(conv(x, ensemble, sample, calculate_log_probs, post_train))
+        
+        x = x.flatten(1)
+
+        for l in self.linears[:-1]:
+            x = self.act(l(torch.cat((x, x_input),1), ensemble, sample, calculate_log_probs, post_train))
+
+        out = self.linears[-1](torch.cat((x, x_input),1), ensemble, sample, calculate_log_probs, post_train)
+        
+        return out
+
+    def kl(self):
+        kl_sum = 0
+        for conv in self.convs:
+            kl_sum = kl_sum + conv.kl
+
+
+        for l in self.linears:
+            kl_sum = kl_sum + l.kl
+
+        return kl_sum
