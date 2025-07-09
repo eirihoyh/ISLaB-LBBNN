ca14577f986c9553e895db247878112667f74f9c
eirihoyh <eirik.hoy@gmail.com>
2025-07-09 15:34:52 +0200

    updated abalone local explain plots

--- implementations/local_explain/abalone/abalone_relu_make_local_explain.py
+++ implementations/local_explain/abalone/abalone_relu_make_local_explain.py
@@ -69,7 +69,10 @@ net.to(device)
 # Abalone variable names
 variable_names = ["Length", "Diameter", "Height", "Whole weight", "Shucked weight", "Viscera weight", "Shell weight", "Sex_I", "Sex_M"]
 
-i = 101
+# i = 101
+# i = 200
+# i = 106
+i = 201
 explain_this = test_dat[i,:-1].reshape(-1, p)[0]
 output_val = test_dat[i,-1]
 print(f"True output value: {int(output_val)}")
@@ -87,15 +90,15 @@ pf.plot_local_explain_piecewise_linear_act(
     ann=False, 
     save_path=f"implementations/local_explain/abalone/imgs/local_explain_{int(output_val)}")
 
-# Empirical explanation
-explain_this = explain_this.reshape(-1, p)
-pf.plot_local_contribution_empirical(
-    net, 
-    explain_this, 
-    sample=True, 
-    median=True, 
-    n_samples=1_000,
-    variable_names=copy.deepcopy(variable_names), 
-    class_names=["regression model"], 
-    magnitude=True,
-    save_path=f"implementations/local_explain/abalone/imgs/local_explain_{int(output_val)}_emp_approach")
+# # Empirical explanation
+# explain_this = explain_this.reshape(-1, p)
+# pf.plot_local_contribution_empirical(
+#     net, 
+#     explain_this, 
+#     sample=True, 
+#     median=True, 
+#     n_samples=1_000,
+#     variable_names=copy.deepcopy(variable_names), 
+#     class_names=["regression model"], 
+#     magnitude=True,
+#     save_path=f"implementations/local_explain/abalone/imgs/local_explain_{int(output_val)}_emp_approach")
